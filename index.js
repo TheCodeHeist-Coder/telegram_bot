5c843f7087b6c9300f1f7ca7a2c44b69240da35c
Rajkumar <thecodeheist01@gmail.com>
2026-03-13 12:17:01 +0530

    basic_telegram_bot_added

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const {Telegraf } = require('telegraf')
+ const{ message }  = require('telegraf/filters')
+
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+
+
+
+bot.start((ctx) => ctx.reply('Welcome to Raj\'s Boat'))    //? when i give /start command it will emit this messsage
+
+bot.command('name', (ctx) => ctx.reply('Hi! My name is Raj'))    // when i give /name then it will tell name
+
+
+bot.on('sticker', (ctx) => ctx.reply('🥰'));  // when somebody send me a sticker then reply with 🥰
+
+bot.on(message('text'), (ctx) => ctx.reply('I don\'t understand humans'))
+
+bot.launch();
